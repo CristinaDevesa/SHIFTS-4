2ac9f4e511a2812510df5198fc3fdc8ccfdb3cbd
Andrea <andrea.laguillo@externo.cnic.es>
2020-05-14 16:04:48 +0200

    Peak Modelling module

---
+++ peak_modeller.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+# Module metadata variables
+__author__ = "Jose Rodriguez"
+__credits__ = ["Jose Rodriguez", "Andrea Laguillo Gómez", "Jesus Vazquez"]
+__license__ = "Creative Commons Attribution-NonCommercial-NoDerivs 4.0 Unported License https://creativecommons.org/licenses/by-nc-nd/4.0/"
+__version__ = "0.0.1"
+__maintainer__ = "Jose Rodriguez"
+__email__ = "jmrodriguezc@cnic.es;andrea.laguillo@cnic.es"
+__status__ = "Development"
+
+# import modules
+import os
+import sys
+import argparse
+import logging
+
+def main(args):
+    '''
+    Main function
+    '''
+
+if __name__ == '__main__':
+
+    # multiprocessing.freeze_support()
+
+    # parse arguments
+    parser = argparse.ArgumentParser(
+        description='Peak Modeller',
+        epilog='''
+        Example:
+            python peak_modeller.py
+
+        ''')
+    parser.add_argument('-i',  '--infile', required=True, help='Input file with the peak file to be filtered')
+    
+    parser.add_argument('-s',  '--slope', help='Slope of DM peak')
+    parser.add_argument('-f',  '--frequency', help='Frequency of DM peak')
+    parser.add_argument('-m',  '--mode', required=True, help='0=filter by slope, 1=filter by frequency, 2=filter by both')
+
+    parser.add_argument('-w',  '--n_workers', type=int, default=4, help='Number of threads/n_workers (default: %(default)s)')    
+    parser.add_argument('-v', dest='verbose', action='store_true', help="Increase output verbosity")
+    args = parser.parse_args()
+
+    # logging debug level. By default, info level
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
+    else:
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    # start main function
+    logging.info('start script: '+"{0}".format(" ".join([x for x in sys.argv])))
+    main(args)
+    logging.info('end script')
