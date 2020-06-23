661d3fb21cfb150b71050d29e20f7f2aaa92fc70
Andrea <andrea.laguillo@externo.cnic.es>
2020-06-23 12:59:59 +0200

    Generate apex list and log

--- PeakSelector.py
+++ PeakSelector.py
@@ -19,24 +19,50 @@ import configparser
 import logging
 import pandas as pd
 import numpy as np
-import concurrent.futures
-from itertools import repeat
 
-def filter_peaks():
+def filter_peaks(df_hist, slope, frequency):
     '''
     Find peaks that are above the thresholds for slope and PSMs.
     '''
-    return
+    # TODO: allow specify slope and count columns in INI?
+    df_hist = df_hist[df_hist['slope1'] >= slope]
+    df_hist = df_hist[df_hist['count'] >= frequency]
+    return df_hist
+
+def peak_apex(bins_df):
+    '''
+    Calculate apex for each peak.
+    '''
+    apex_list = []
+    for i in range(1, len(bins_df)):
+        if bins_df.loc[i, 'slope2'] is not None:
+            i1 = bins_df.loc[i-1, 'bin']
+            i2 = bins_df.loc[i, 'bin']
+            # Check intervals are consecutive, and there is a change in sign of slope2
+            if i1.right == i2.left and bins_df.loc[i, 'slope2'] < 0 and bins_df.loc[i-1, 'slope2'] >= 0:
+                peak = pd.Series([bins_df.loc[i-1, 'midpoint'], np.nan, bins_df.loc[i, 'midpoint']],
+                                  index=[bins_df.loc[i-1, 'slope2'], 0, bins_df.loc[i, 'slope2']])
+                peak = peak.interpolate(method='index')
+                apex_list.append(peak[0])
+    return apex_list
 
 def main(args):
     '''
     Main function
     '''
     
-    logging.info("read input DMTable")
-    df = pd.read_csv(infile, sep="\t", float_precision='high')
-    #filter
+    logging.info("Reading input file...")
+    df_hist = pd.read_csv(args.histogram, sep="\t", float_precision='high')
+    logging.info("Filtering...")
+    df_hist = filter_peaks(df_hist, #slope, #frequency)
+    apex_list = peak_apex(df_hist) #TODO: could there be problems bc of filtering?
     # write apex list in txt
+    logging.info("Writing apex list...")
+    outfile = args.infile[:-8] + '_ApexList.txt'
+    with open(outfile, 'w') as f:
+        for apex in apex_list:
+            f.write("%s\n" % apex)
+    logging.info("Peak Selection finished")
 
 if __name__ == '__main__':
 
@@ -53,8 +79,7 @@ if __name__ == '__main__':
         
     defaultconfig = os.path.join(os.path.dirname(__file__), "config/PeakModeller.ini")
     
-    parser.add_argument('-i', '--infile', required=True, help='DMTable to be filtered')
-    parser.add_argument('-j', '--infile', required=True, help='DMHistogram with PSM and slope information')
+    parser.add_argument('-i', '--infile', required=True, help='DMHistogram to be filtered')
     
     parser.add_argument('-s', '--slope', help='Threshold for slope of DM peak')
     parser.add_argument('-f', '--frequency', help='Threshold for number of PSMs')
@@ -67,12 +92,6 @@ if __name__ == '__main__':
     # parse config
     config = configparser.ConfigParser(inline_comment_prefixes='#')
     config.read(args.config)
-    if args.bins is not None:
-        config.set('PeakModeller', 'bins', str(args.bins))
-        config.set('Logging', 'create_ini', '1')
-    if args.points is not None:
-        config.set('PeakModeller', 'points', str(args.points))
-        config.set('Logging', 'create_ini', '1')
     if args.slope is not None:
         config.set('PeakSelector', 'slope', str(args.slope))
         config.set('Logging', 'create_ini', '1')
@@ -83,18 +102,22 @@ if __name__ == '__main__':
     if config.getint('Logging', 'create_ini') == 1:
         with open(os.path.dirname(args.infile) + '/DMcalibrator.ini', 'w') as newconfig:
             config.write(newconfig)
-            
-    # TODO: add mode to log!
 
     # logging debug level. By default, info level
+    log_file = outfile = args.infile[:-4] + '_log.txt'
+    log_file_debug = outfile = args.infile[:-4] + '_log_debug.txt'
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s - %(levelname)s - %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
+                            datefmt='%m/%d/%Y %I:%M:%S %p',
+                            handlers=[logging.FileHandler(log_file_debug),
+                                      logging.StreamHandler()])
     else:
         logging.basicConfig(level=logging.INFO,
                             format='%(asctime)s - %(levelname)s - %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
+                            datefmt='%m/%d/%Y %I:%M:%S %p',
+                            handlers=[logging.FileHandler(log_file),
+                                      logging.StreamHandler()])
 
     # start main function
     logging.info('start script: '+"{0}".format(" ".join([x for x in sys.argv])))
