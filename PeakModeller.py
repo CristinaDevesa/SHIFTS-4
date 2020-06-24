1b729c3b8c41f59966ce3fb562d8dcc729aa3dd7
Andrea <andrea.laguillo@externo.cnic.es>
2020-06-24 12:01:27 +0200

    smoothing options
    Number of points used for smoothing can now be different from number of points used for slope calculation. Smoothing can now be toggled on and off.

--- PeakModeller.py
+++ PeakModeller.py
@@ -20,7 +20,6 @@ import logging
 import pandas as pd
 import numpy as np
 import concurrent.futures
-from itertools import repeat
 pd.options.mode.chained_assignment = None  # default='warn'
 
 #infile = r"C:\Users\Andrea\Desktop\SHIFTS-4\testing\cXcorr_Len_Rank_Results_TargetData_Calibration.txt"
@@ -100,21 +99,21 @@ def linear_regression(bin_subset, smoothed, second_derivative):
     else:
         return working_slope, intercept
 
-def smoothing(bins_df, points):
+def smoothing(bins_df, spoints):
     '''
     Calculate the slope (first derivative) for each bin. Calculate new smoothed
     value for the midpoint using the linear regression line.
     '''
     bins_df['smooth_count'] = None
-    for i in range(points, len(bins_df)-points): #TODO: handle leftovers at start/end
+    for i in range(spoints, len(bins_df)-spoints): #TODO: handle leftovers at start/end
         #working_bin = bins_df.loc[i]
-        bin_subset = bins_df[i-points:i+points+1]
+        bin_subset = bins_df[i-spoints:i+spoints+1]
         working_slope, intercept = linear_regression(bin_subset, False, False)
         bins_df.loc[i, 'smooth_count'] = intercept + (working_slope*bins_df.loc[i, 'midpoint'])
     bins_df[["smooth_count"]] = bins_df[["smooth_count"]].apply(pd.to_numeric)
     return bins_df
 
-def first_derivative(bins_df, points):
+def first_derivative(bins_df, points, spoints):
     '''
     Calculate the slope (first derivative) for each bin.
     Returns the slope of the linear regression line through data points in
@@ -124,21 +123,32 @@ def first_derivative(bins_df, points):
     Known_y's  Bins. An array or cell range of numeric dependent data points.
     Known_x's  Count. The set of independent data points.
     '''
-    bins_df = smoothing(bins_df, points)
+    if spoints > 0: #smoothing
+        bins_df = smoothing(bins_df, spoints)
+        j = 2
+    else: #no smoothing
+        j = 1
     bins_df['slope1'] = None
-    for i in range(points*2, len(bins_df)-points*2): #TODO: handle leftovers at start/end
+    for i in range(points*j, len(bins_df)-points*j): #TODO: handle leftovers at start/end
         #working_bin = bins_df.loc[i]
         bin_subset = bins_df[i-points:i+points+1]
-        bins_df.loc[i, 'slope1'] = linear_regression(bin_subset, True, False)
+        if spoints > 0:
+            bins_df.loc[i, 'slope1'] = linear_regression(bin_subset, True, False)
+        else:
+            bins_df.loc[i, 'slope1'] = linear_regression(bin_subset, False, False)[0] #slope only
     bins_df[['slope1']] = bins_df[['slope1']].apply(pd.to_numeric)
     return bins_df
 
-def second_derivative(bins_df, points):
+def second_derivative(bins_df, points, spoints):
     '''
     Calculate the second derivative for each bin.
     '''
+    if spoints > 0: #smoothed
+        j = 3
+    else: #not smoothed
+        j = 2
     bins_df['slope2'] = None
-    for i in range(points*3, len(bins_df)-points*3): #TODO: handle leftovers at start/end
+    for i in range(points*j, len(bins_df)-points*j): #TODO: handle leftovers at start/end
         bin_subset = bins_df[i-points:i+points+1]
         bins_df.loc[i, 'slope2'] = linear_regression(bin_subset, False, True)
     bins_df[['slope2']] = bins_df[['slope2']].apply(pd.to_numeric)           
@@ -169,8 +179,12 @@ def main(args):
     df, bins_df = generate_histogram(df, float(config._sections['PeakModeller']['bins']))
     # calculate derivatives
     #grouped_bins_df = bins_df.groupby(['bin'])
-    bins_df = first_derivative(bins_df, int(config._sections['PeakModeller']['points'])//2)  #does 1st smoothing pass and 2nd normal pass
-    bins_df = second_derivative(bins_df, int(config._sections['PeakModeller']['points'])//2)
+    bins_df = first_derivative(bins_df, #does 1st smoothing pass and 2nd normal pass
+                               int(config._sections['PeakModeller']['points'])//2,
+                               int(config._sections['PeakModeller']['spoints'])//2)  
+    bins_df = second_derivative(bins_df,
+                                int(config._sections['PeakModeller']['points'])//2,
+                                int(config._sections['PeakModeller']['spoints'])//2)
         # check which bins pass
     logging.info("Writing output files...")
     # write DMhistogram
@@ -201,6 +215,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-b', '--bins', help='Width of the bins')
     parser.add_argument('-p', '--points', help='Number of points (bins) to use for slope calculation')
+    parser.add_argument('-s', '--spoints', help='Number of points (bins) to use for smoothing')
 
     parser.add_argument('-w',  '--n_workers', type=int, default=4, help='Number of threads/n_workers (default: %(default)s)')    
     parser.add_argument('-v', dest='verbose', action='store_true', help="Increase output verbosity")
@@ -214,6 +229,9 @@ if __name__ == '__main__':
         config.set('Logging', 'create_ini', '1')
     if args.points is not None:
         config.set('PeakModeller', 'points', str(args.points))
+        config.set('Logging', 'create_ini', '1')
+    if args.spoints is not None:
+        config.set('PeakModeller', 'spoints', str(args.spoints))
         config.set('Logging', 'create_ini', '1')
     # if something is changed, write a copy of ini
     if config.getint('Logging', 'create_ini') == 1:
